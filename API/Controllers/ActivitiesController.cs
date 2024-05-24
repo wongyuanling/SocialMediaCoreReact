1ac36e1e1c44219c2ccde3158976779193f973af
wongyuanling <47958742+wongyuanling@users.noreply.github.com>
2024-05-25 00:52:01 +0800

    Create initial structures of backend API

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,31 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System.Diagnostics;
+
+namespace API.Controllers;
+
+public class ActivitiesController : BaseAPIController
+{
+    private readonly DataContext _context;
+    public ActivitiesController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Domain.Activity>>> GetActivities()
+    { //api/activities
+        return await _context.Activities.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Domain.Activity>> GetActivity(Guid id)
+    { //api/activities/id
+        return await _context.Activities.FindAsync(id);
+
+    }
+
+
+}
---
+++ API/Controllers/BaseAPIController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BaseAPIController : ControllerBase { 
+
+}
